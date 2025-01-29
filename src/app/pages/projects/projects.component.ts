34f8b654b1e1b8d29ac49e03c424be0caa364e43
Jerry Liu <ljliu89@gmail.com>
2025-01-28 23:59:49 -0800

    Edit pages

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,14 +10,56 @@ import { CommonModule } from '@angular/common';
 
 export class ProjectsComponent {
   images = [
-    'assets/1.jpg', // Replace with your image paths
+    'assets/1.jpg',
     'assets/2.jpg',
     'assets/3.jpg',
   ];
+  imageTexts = [
+    "What To Wear App",
+    "News Explorer",
+    "Upcoming",
+  ]
 
-  activeIndex = 0; // Start with the first image as active
+  logos =[
+    'assets/react.svg',
+    'assets/angular.svg',
+    'assets/javascript.svg',
+    'assets/typescript.svg',
+    'assets/nodejs.svg',
+    'assets/tailwind.svg',
+    'assets/python.svg',
+    'assets/html.svg',
+    'assets/css.svg',
+    'assets/github.svg',
+    'assets/mongodb.svg',
+    'assets/google-cloud.svg',
+    'assets/aws.svg',
+    'assets/ruby.svg',
+    'assets/rails.svg',
+    'assets/postman.svg',
+    'assets/postgresql.svg',
+    'assets/mysql.svg',
+  ]
+
+  activeIndex = 0;
+  isSmallScreen: boolean = false;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isSmallScreen = window.innerWidth < 768;
+  }
 
   setActiveIndex(index: number): void {
-    this.activeIndex = index;
+    if (!this.isSmallScreen) {
+      this.activeIndex = index;
+    }
   }
 }
