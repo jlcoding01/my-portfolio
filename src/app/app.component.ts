7906e07b117fb5fbe865ff0e0e0befdcc48c86ce
Jerry Liu <ljliu89@gmail.com>
2025-01-29 23:29:46 -0800

    Edit pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {  RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,7 +8,7 @@ import { AboutComponent } from './pages/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent,FooterComponent, HomeComponent, ProjectsComponent, AboutComponent],
+  imports: [ NavBarComponent, HomeComponent, ProjectsComponent, AboutComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {path: '', component: HomeComponent},
+  // {path:'projects', component: ProjectsComponent},
+  // {path:'about', component: AboutComponent}
+];
